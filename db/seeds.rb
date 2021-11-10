5bcbf9d7bf46db1b676b0ca4a7fe23f38e5b7f5c
legnachavez <legna.ch18@gmail.com>
2021-11-10 15:35:06 +0100

    hello

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+category_array = ["chinese", "italian", "japanese", "french", "belgian"]
+
+puts "Restaurant"
+5.times do
+  Restaurant.create(name: Faker::Restaurant.name, address: Faker::Restaurant.address, category: category_array.sample, phone_number: Faker::PhoneNumber.phone_number)
+end
+
+puts "finish!"
